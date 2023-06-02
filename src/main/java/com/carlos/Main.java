b495a79f904d6ce75794b8a54755b0c15541c4e2
Carlos Flores <carlosfloresalex@gmail.com>
2023-06-02 02:34:47 -0400

    added all crud operations for customer

--- src/main/java/com/carlos/Main.java
+++ src/main/java/com/carlos/Main.java
@@ -2,11 +2,10 @@ package com.carlos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootApplication
 @RestController
@@ -23,8 +22,40 @@ public class Main {
         SpringApplication.run(Main.class, args);
     }
 
+    record NewCustomerRequest(
+            String name,
+            String email,
+            Integer age
+    ) {}
+
     @GetMapping
     public List<Customer> getCustomers() {
         return customerRepository.findAll();
+    }
+
+    @PostMapping
+    public void addCustomer(@RequestBody NewCustomerRequest request) {
+        Customer customer = new Customer();
+        customer.setName(request.name());
+        customer.setEmail(request.email());
+        customer.setAge(request.age());
+        customerRepository.save(customer);
+    }
+
+    @DeleteMapping("{customerId}")
+    public void delteCustomer(@PathVariable("customerId") Integer id) {
+        customerRepository.deleteById(id);
+    }
+
+    @PutMapping("{customerId}")
+    public void updateCustomer(@PathVariable("customerId") Integer id, @RequestBody NewCustomerRequest request) {
+        customerRepository.findById(id)
+                .map(customer -> {
+                    if(request.age != null) customer.setAge(request.age);
+                    if(request.name != null || request.name.length() > 0) customer.setName(request.name);
+                    if(request.email != null || request.email.length() > 0) customer.setEmail(request.email);
+
+                    return customerRepository.save(customer);
+                });
     }
 }
